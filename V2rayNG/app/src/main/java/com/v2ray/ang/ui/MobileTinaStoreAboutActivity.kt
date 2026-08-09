a2f7969ae604a1a12c5aa61d6f9663c251646e71
VPN963 <120357728+VPN963@users.noreply.github.com>
2026-08-09 11:10:43 +0330

    Add MobileTina store about page

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MobileTinaStoreAboutActivity.kt
@@ -0,0 +1,34 @@
+package com.v2ray.ang.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.v2ray.ang.R
+import com.v2ray.ang.util.Utils
+
+class MobileTinaStoreAboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobiletina_store_about)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_store_about)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        toolbar.setNavigationOnClickListener { finish() }
+
+        findViewById<View>(R.id.card_instagram_1).setOnClickListener {
+            Utils.openUri(this, "https://www.instagram.com/mobile.tina/")
+        }
+        findViewById<View>(R.id.card_instagram_2).setOnClickListener {
+            Utils.openUri(this, "https://www.instagram.com/mobile.tina2/")
+        }
+        findViewById<View>(R.id.card_instagram_3).setOnClickListener {
+            Utils.openUri(this, "https://www.instagram.com/mobile.tinaa/")
+        }
+        findViewById<View>(R.id.card_developer).setOnClickListener {
+            Utils.openUri(this, "https://t.me/vpn963")
+        }
+    }
+}
